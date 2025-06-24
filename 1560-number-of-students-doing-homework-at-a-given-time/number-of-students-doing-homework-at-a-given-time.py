99c3fc9d9ca6abbd1ca0b92f8e70204e352a6af4
Brandon Viaje <viajebrandon@gmail.com>
2025-06-24 18:12:55 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (90.11%) - LeetSync

---
+++ 1560-number-of-students-doing-homework-at-a-given-time/number-of-students-doing-homework-at-a-given-time.py
@@ -0,0 +1,8 @@
+class Solution:
+    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+        result = 0
+        for i in range(len(startTime)):
+            if (startTime[i] <= queryTime <= endTime[i]):
+                result += 1
+
+        return result
