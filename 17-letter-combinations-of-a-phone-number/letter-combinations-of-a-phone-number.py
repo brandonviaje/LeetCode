60173fb66b4b048dab899d7cfd000d569e32d34e
Brandon Viaje <viajebrandon@gmail.com>
2025-06-26 19:46:57 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (22.80%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,33 +1,39 @@
 class Solution:
+    
 
-    def backTrack(self, index, combination, phoneMap, digits, result):
-
-        # if index reaches last digit copy string to result
-        if index == len(digits):
-            result.append(combination[:])
-            return
-
-        # generate all possible combos, backtrack after each iteration
-        for letter in phoneMap[digits[index]]:
-            self.backTrack(index + 1, combination + letter, phoneMap, digits, result)
-
-    def letterCombinations(self, digits: str) -> List[str]:
-        # if empty string return empty list
-        if not digits:
-            return []
-
-        phoneMap = {
-            '2': ['a', 'b', 'c'],
-            '3': ['d', 'e', 'f'],
-            '4': ['g', 'h', 'i'],
-            '5': ['j', 'k', 'l'],
-            '6': ['m', 'n', 'o'],
-            '7': ['p', 'q', 'r', 's'],
-            '8': ['t', 'u', 'v'],
-            '9': ['w', 'x', 'y', 'z'],
-        }
-
-        result = []
-        self.backTrack(0, "", phoneMap, digits, result)
-        return result
             
+    def letterCombinations(self, digits: str) -> List[str]:
+        """
+        generate all possible = backtracking
+        if u want to explore all possible combos = backtrack
+        base case
+        
+        """
+        # if digits is empty return nothing
+        if len(digits) == 0:
+            return []
+            
+        phone = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y","z"]
+        }
+        def backtrack(index, path):
+            #base case
+            if index == len(digits):
+                result.append(path[:])
+                return
+                
+            # loop through each string backtrack after
+            for l in phone[digits[index]]:
+                backtrack(index + 1, path + l)
+            
+        result = []
+        path = ""
+        backtrack(0,path)
+        return result
