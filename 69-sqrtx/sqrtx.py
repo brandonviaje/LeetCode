c8fc385b32d1025728567643918a89f4256400f4
Brandon Viaje <viajebrandon@gmail.com>
2025-06-26 22:02:31 -0400

    Time: 7 ms (27.92%) | Memory: 18 MB (10.68%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,18 +1,34 @@
+import math
+
 class Solution:
     def mySqrt(self, x: int) -> int:
-
         left = 0
         right = x
         result = 0
-        while left <= right:
-            middle = (left + right) // 2
-
-            if middle * middle == x:
-                return middle
-            elif middle * middle < x:
-                result = middle
-                left = middle + 1
-            else:
-                right = middle - 1
         
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if mid * mid== x:
+                return mid
+            elif ( mid * mid) < x:
+                result = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+                
         return result
+
+        
+        # for i in range(1000):
+        #     print(f"A: {endpoint_a} B: {endpoint_b} Midpoint: {midpoint}")
+
+        #     if abs(midpoint * midpoint - target) < epsilon:
+        #         return math.floor(midpoint)
+        #     elif (midpoint * midpoint) > target:
+        #         endpoint_b = midpoint
+        #         midpoint = (endpoint_a + endpoint_b) / 2
+        #     else:
+        #         endpoint_a = midpoint
+        #         midpoint = (endpoint_a + endpoint_b) / 2
+                
