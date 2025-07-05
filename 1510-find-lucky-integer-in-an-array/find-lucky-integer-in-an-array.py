e9cb3dee14cde4f2fac09f845b2f640b6aafc453
Brandon Viaje <viajebrandon@gmail.com>
2025-07-05 13:36:42 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (77.76%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
@@ -1,14 +1,10 @@
 class Solution:
     def findLucky(self, arr: List[int]) -> int:
-        count = Counter(arr)
-        maxKey = 0
+        track = Counter(arr)
+        maxNum = -1
 
-        for key,value in count.items():
-            # if key is bigger than max key and freq is same set that as max
-            if key > maxKey and key == value:
-                maxKey = key
+        for num, freq in track.items():
+            if freq == num and num > maxNum:
+                maxNum = num
 
-        if maxKey != 0:
-            return maxKey
-        else:
-            return -1
+        return maxNum
