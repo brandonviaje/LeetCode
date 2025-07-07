e203352366fba3c8998215871f7bb3f704f1696b
Brandon Viaje <viajebrandon@gmail.com>
2025-07-07 16:15:40 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (33.78%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,53 +1,34 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
-        [1,0,2]
-         ^ ^
+        Do not return anything, modify nums in-place instead.
+        """
+        left,right = 0, len(nums)-1
+        zeroes = 0
+
+
+        """
+        [0, 0, 1, 1, 2, 2]
+               ^
+                   ^
+               ^
+
+        [0,1,2]
            ^
-
+           ^
+           
+           ^ 
         """
-        left, right = 0, len(nums)-1
-        current = 0
-        while current <= right:
-            if nums[current] == 0:
-                nums[left],nums[current] = nums[current],nums[left]
+
+        while left <= right:
+            pass
+            if nums[left] == 0:
+                nums[left],nums[zeroes] = nums[zeroes],nums[left]
+                zeroes += 1
                 left += 1
-                current += 1
-                print(nums)
-            elif nums[current] == 1:
-                current += 1
+            elif nums[left] == 1:
+                left += 1
             else:
-                nums[right],nums[current] = nums[current], nums[right]
+                nums[right],nums[left] = nums[left],nums[right] 
                 right -= 1
-      
-     
-                        
-    
         
-        
-        
-        
-        
-        
-        
-        
-        
-        """
-        
-        for i in range(len(nums)):
-            left = nums[i]
-            right = nums[len(nums)-i-1]
-            
-            if left == 2 and right == 2:
-                if
-            
-                
-                6-0-1=5
-                6-1-1=4
-                6-2-1=3
-                
-                
-        [2, 1, 2, 0, 1, 2]
-        [1, 1, 2, 0, 2, 2]
-
-        """
