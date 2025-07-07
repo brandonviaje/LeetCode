34f5bf9416a5252760cd308f101b252f4c372ec1
Brandon Viaje <viajebrandon@gmail.com>
2025-07-07 18:39:44 -0400

    Time: 65 ms (70.59%) | Memory: 19.3 MB (65.57%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.py
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.py
@@ -1,50 +1,35 @@
 from collections import deque
-
 class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
-        
-        rows = len(maze)
-        cols = len(maze[0])
-        queue = deque([(entrance[0],entrance[1],0)])
-        seen = set([tuple(entrance)])
+        ROWS = len(maze)
+        COLS = len(maze[0])
+        queue = deque([(entrance[0],entrance[1])])
+        distance = 0
+        maze[entrance[0]][entrance[1]] = "+" # mark as seen
 
         while queue:
-            
-            # pop current row,col and step
-            cand_row,cand_col,steps = queue.popleft()
+            for _ in range(len(queue)):
+                # process node
+                cand_row, cand_col = queue.popleft()
 
-            # check if player is at border and not at entrance
-            if [cand_row,cand_col] != entrance and (cand_row == 0 or cand_row == rows - 1 or cand_col == 0 or cand_col == cols -1):
-                return steps
+                # check if it is at the border but not at entrance
+                if [cand_row,cand_col] != entrance and (cand_row == 0 or cand_row == ROWS-1 or cand_col == 0 or cand_col == COLS-1):
+                    return distance
 
-            # process adjacent neighbs
-            neighbs = [
-                (cand_row - 1,cand_col),
-                (cand_row + 1,cand_col),
-                (cand_row,cand_col - 1),
-                (cand_row,cand_col + 1)
-            ]
-
-            for neighb_row,neighb_col in neighbs:
-                # check for index bounds
-                if neighb_row < 0 or neighb_row >= rows:
-                    continue
-
-                if neighb_col < 0 or neighb_col >= cols:
-                    continue
-
-                # continue if already visited
-                if (neighb_row, neighb_col) in seen:
-                    continue
-
-                # continue if we its a wall 
-                if maze[neighb_row][neighb_col] == '+':
-                    continue
-
-                # after passing all checks, add valid node and increment step
-                queue.append((neighb_row,neighb_col,steps + 1))
-                seen.add((neighb_row,neighb_col)) 
+                neighbs = [
+                    (cand_row - 1, cand_col),
+                    (cand_row + 1, cand_col),
+                    (cand_row, cand_col - 1),
+                    (cand_row, cand_col + 1)
+                ]
+                
+                for neighb_row,neighb_col in neighbs:
+                    # check if in bound, and is an empty cell and not visited already
+                    if 0<=neighb_row<ROWS and 0<=neighb_col<COLS and maze[neighb_row][neighb_col] == ".":
+                        maze[neighb_row][neighb_col] = "+" # mark as seen
+                        queue.append((neighb_row,neighb_col)) # add to q
+            distance += 1
 
         return -1
 
-        # T  O(rows * cols) S O(rows * cols)
+    
