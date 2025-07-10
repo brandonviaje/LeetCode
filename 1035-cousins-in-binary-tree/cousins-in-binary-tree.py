f9a9e2602ab65a40db323c871c1bda67c5398d8b
Brandon Viaje <viajebrandon@gmail.com>
2025-07-10 18:58:53 -0400

    Time: 0 ms (100.00%) | Memory: 18.1 MB (10.19%) - LeetSync

--- 1035-cousins-in-binary-tree/cousins-in-binary-tree.py
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.py
@@ -6,38 +6,40 @@
 #         self.right = right
 from collections import deque
 class Solution:
-
-    def DFS(self,node,target,depth):
-        if not node:
-            return
-
-        if node.val == target:
-            return depth
-        
-        # check left and right if needed
-        return self.DFS(node.left, target, depth + 1) or self.DFS(node.right, target, depth + 1)
-     
-
     def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
+        """
+        map current node val to the parent node using BFS and a  hashmap
+        identify the depths of the node and check if the left depth == right depth and parent[left.val] != parent[right.val]!= 0
+        """
+
+        def dfs(root,target,depth):
+            # base case
+            if not root:
+                return  
+            # if found return depth
+            if root.val == target:
+                return depth
+            # explore left or right subtrees for val
+            return dfs(root.left,target,depth + 1) or dfs(root.right,target,depth+1)
+
         queue = deque([root])
-        children = {}
+        parent = {}
 
-        # BFS to build children and parent relationship
         while queue:
-            level = len(queue)
-            for _ in range(level):
-                cand = queue.popleft()
-                if cand.left:
-                    queue.append(cand.left)
-                    children[cand.left.val] = cand # map left value to parent node
-                if cand.right:
-                    queue.append(cand.right)
-                    children[cand.right.val] = cand # map right value to parent node
+            
+            cand = queue.popleft()
 
-        # find depth of x and y
-        left_depth =  self.DFS(root,x,0)
-        right_depth = self.DFS(root,y,0)
+            # map values to corresponding parent node
+            if cand.left:
+                parent[cand.left.val] = cand
+                queue.append(cand.left)
+            if cand.right:
+                parent[cand.right.val] = cand
+                queue.append(cand.right)
 
-        # return if cousin or not
-        return(left_depth == right_depth) and children[x] != children[y]
-        # T O(n) S O(n)
+        # DFS until you hit the target nodes, get depth from it
+        x_depth = dfs(root,x,0)
+        y_depth = dfs(root,y,0)
+
+        # return if its a parent
+        return x_depth == y_depth and parent[x] != parent[y]
