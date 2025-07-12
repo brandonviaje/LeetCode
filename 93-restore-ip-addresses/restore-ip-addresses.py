9fa891be09d166a8c5e46c2555bc48d1bcf92d80
Brandon Viaje <viajebrandon@gmail.com>
2025-07-12 16:40:52 -0400

    Time: 2 ms (67.01%) | Memory: 17.9 MB (48.54%) - LeetSync

--- 93-restore-ip-addresses/restore-ip-addresses.py
+++ 93-restore-ip-addresses/restore-ip-addresses.py
@@ -11,28 +11,31 @@ class Solution:
 
             return 0<=int(s)<=255
 
-        def backtrack(index,dots):
+        def backtrack(index,segmentsLeft):
             # BASE CASE
-            if index == len(s) and dots == 0:
+            if index == len(s) and segmentsLeft == 0:
                 addresses.append([n for n in path])
                 return
 
             for i in range(index,len(s)):
                 # check if its a valid number (0-255)
                 if validNum(s[index:i+1]):
+                    # PRUNE: CHECK IF THERE'S ENOUGH  OR TOO MANY CHARS
+                    if len(s) - (i + 1) > (segmentsLeft - 1) * 3:
+                        continue
+                    if len(s) - (i + 1) < (segmentsLeft - 1): 
+                        continue
+                    
                     # ADD TO SOMETHING  
                     path.append(s[index:i+1])
 
                     # RECURSE
-                    backtrack(i+1,dots-1)
+                    backtrack(i+1,segmentsLeft-1)
 
                     # UNDO
                     path.pop()
 
         backtrack(0,4)
         result = []
-        
-        for add in addresses:
-            result.append(".".join(add))
 
-        return result
+        return [".".join(a) for a in addresses]
