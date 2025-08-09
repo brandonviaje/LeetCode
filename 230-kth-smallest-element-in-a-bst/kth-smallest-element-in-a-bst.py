a7bf6d84dae889fdecf1579a2493824efe16cdaf
Brandon Viaje <viajebrandon@gmail.com>
2025-08-09 14:09:53 -0400

    Time: 5 ms (8.20%) | Memory: 21.1 MB (72.55%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -6,28 +6,26 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        """
-        in-order traversal to collect the ordered nodes
-
-        after, that use a loop to decrement k until it hits 0, take that index and return it
-        """
-
-        def inOrder(root,result):
-            # base case
-            if not root:
-                return 
-
-            inOrder(root.left,result)
-            result.append(root.val)
-            inOrder(root.right,result)
-
-        result = []
-        inOrder(root,result) # in order traversal on a BST gives ordered list
         i = 0
+        result = -1
 
-        # scan through array until k is at 0, then we know we are at the i+1th index
-        while k > 0:  
-            k -= 1
+        def dfs(root):
+            nonlocal i
+            nonlocal result
+            if not root:
+                return
+
+            dfs(root.left)
+
             i += 1
 
-        return result[i-1]
+            # if i is equal to k, we found our kth smallest since we are doing in order
+            if i == k:
+                result = root.val
+                return
+
+            dfs(root.right)
+
+        dfs(root)
+        return result
+        
