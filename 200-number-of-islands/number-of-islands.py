422eef5be583b41f971d8ad4c69b8d8fc9f515ff
Brandon Viaje <viajebrandon@gmail.com>
2025-09-04 14:50:06 -0400

    Time: 212 ms (88.65%) | Memory: 27.5 MB (94.82%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,50 +1,63 @@
-class Solution:
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        """
+        count the number of islands by doing a DFS to explore all horizontally and vertically adjacent cells
+        DFS for graphs using matrix would be O(m*n) as you have to iterate through the whole grid
+        U can either keep track of the coords of each cell by using a set or overwriting it on the grid
+        """
 
-    def DFS(self,grid,seen,row,col):
+        # DFS so that we can mark all neighboring cells
+        def DFS(row,col):
+            # mark the starting point as seen
+            grid[row][col] = "2"
 
-        stack = [(row,col)]
-        seen.add((row,col))
+            # stack for DFS
+            stack = [(row,col)]
 
-        while stack:
-            cand_row,cand_col = stack.pop()
+            # while the stack isnt empty, explore all neighboring cells and mark it as seen so you dont
+            # encounter any infinite loops
+            while stack:
+                cand_row,cand_col = stack.pop()
 
-            # horizontal and vertically adjq
-            neighbs = [
-            (cand_row + 1,cand_col),
-            (cand_row - 1,cand_col),
-            (cand_row,cand_col + 1),
-            (cand_row,cand_col - 1)
-            ]
+                # neighboring cells can only be vertically and horizontally adjacent to each other
+                neighbs = [
+                    (cand_row - 1,cand_col),
+                    (cand_row + 1,cand_col),
+                    (cand_row,cand_col - 1),
+                    (cand_row,cand_col + 1)
+                ]
 
-            for neighb_row, neighb_col in neighbs:
+                # this would only be O(1) since you have a fixed size of 4 always
+                for nr,nc in neighbs:
+                    # what do we have to check for it?
+                    # bounds check, check if its water or seen
 
-                # check for out of bounds
-                if neighb_row < 0 or neighb_row >= len(grid):
-                    continue
+                    if 0<=nr<ROWS and 0<=nc<COLS and grid[nr][nc] == "1":
+                        # add cell to the stack to explore even further, then mark it as seen
+                        stack.append((nr,nc))
+                        grid[nr][nc] = "2"
 
-                if neighb_col < 0 or neighb_col >= len(grid[0]):
-                    continue
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        islands = 0
 
-                # if already seen
-                if (neighb_row,neighb_col) in seen:
-                    continue    
+        for row in range(ROWS):
+            for col in range(COLS):
+                # if its equal to 1, it is an unvisited island
+                if grid[row][col] == "1":
+                    DFS(row,col) # keep track of all valid islands cells thats part of the island rn
+                    islands += 1 # iterate islands 
 
-                # node hasn't been visited and is valid
-                if grid[neighb_row][neighb_col] == "1":
-                    stack.append((neighb_row,neighb_col))
-                    seen.add((neighb_row,neighb_col))
-                
+        return islands
 
-    def numIslands(self, grid: List[List[str]]) -> int:
-        result = 0
-        # add a seen array to avoid inf loops when exploring 
-        seen = set()
-
-        for row in range(len(grid)):
-            for col in range(len(grid[0])):
-                # check if we haven't seen this node yet and if it is land
-                if grid[row][col] == "1" and (row,col) not in seen:
-                    self.DFS(grid,seen,row,col) # DFS to explore valid adjacent nodes
-                    result += 1
-
-        return result
+        """
+        [
+        ["1","1","1","1","0"],
+        ["1","1","0","1","0"],
+        ["1","1","0","0","0"],
+        ["0","0","0","0","0"]]
+        """
