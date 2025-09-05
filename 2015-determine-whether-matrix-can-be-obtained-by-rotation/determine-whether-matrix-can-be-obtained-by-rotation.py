c78fe909b33bc7e61a9b1107b9759111ceec77dc
Brandon Viaje <viajebrandon@gmail.com>
2025-09-05 18:09:54 -0400

    Time: 0 ms (100.00%) | Memory: 12.5 MB (16.78%) - LeetSync

---
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def findRotation(self, mat, target):
+        """
+        :type mat: List[List[int]]
+        :type target: List[List[int]]
+        :rtype: bool
+        """
+
+        # rotate the first matrix 90 degrees and check if they look the same as target matrix
+
+        # perform a vertical reversal 3 times, check if you ever get a matching pair
+        for i in range(4):
+            top = 0
+            bottom = len(mat) - 1
+
+            while top < bottom:
+                for col in range(len(mat)):
+                    # perform the swap
+                    mat[top][col],mat[bottom][col] = mat[bottom][col],mat[top][col]
+
+                top += 1
+                bottom -= 1
+
+            # transpose the matrix
+            for row in range(len(mat)):
+                for col in range(row+1,len(mat)):
+                    mat[row][col],mat[col][row] = mat[col][row],mat[row][col]
+
+            # if the rotated matrix is the same as target return True
+            if mat == target:
+                return True
+
+        # Else return false
+        return False
+        
