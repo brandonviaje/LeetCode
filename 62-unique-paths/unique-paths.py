169d6759cd42353550113e7852c2663b6c937d47
Brandon Viaje <viajebrandon@gmail.com>
2025-10-28 16:33:33 +0000

    Time: 3 ms (22.40%) | Memory: 18 MB (26.65%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,19 +1,27 @@
 class Solution:
     # Reccurence Relation: DP(i,j) = DP(i+1,j) + DP(i,j+1)
+    """
+    can only move down or right
+    Let DP(i,j) be the number of possible unique paths to reach end
+    DP(i,j) = DP(i+1,j) + DP(i,j+1)
+    """
     
     def uniquePaths(self, m: int, n: int) -> int:
-        # Cache result of subproblems to better the TC
-        @lru_cache(maxsize = None)
-        def dfs(row,col):
-            # Out of bounds check
+        memo = {}
+        def DFS(row,col):
+            # check for precomputed solutions
+            if (row,col) in memo:
+                return memo[(row,col)]
+
+            # check if reached goal
+            if (row,col) == (m-1,n-1):
+                return 1
+            # base case, out of bounds
             if row >= m or col >= n:
                 return 0
 
-            # We have found a unique path
-            if (row,col) == (m-1,n-1):
-                return 1
+            # recurrence relation
+            memo[(row,col)] = DFS(row+1,col) + DFS(row,col+1)
+            return memo[(row,col)]
 
-            # Explore down or right
-            return dfs(row+1,col) + dfs(row,col+1)
-
-        return dfs(0,0)
+        return DFS(0,0)
