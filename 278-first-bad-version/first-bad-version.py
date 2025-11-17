5245cde560807384fd872c1eb1a9ec27210fd55e
Brandon Viaje <viajebrandon@gmail.com>
2025-11-17 10:19:04 -0500

    Time: 34 ms (76.12%) | Memory: 17.6 MB (92.16%) - LeetSync

--- 278-first-bad-version/first-bad-version.py
+++ 278-first-bad-version/first-bad-version.py
@@ -5,32 +5,20 @@ class Solution:
     def firstBadVersion(self, n: int) -> int:
         
         """
-        - want to find the very first  bad version of product
-        - given n version
-        - binary search to minimize num of API calls
-        - if mid is >= right and isBadVersion() set that as result
+        binary search to find the very leftmost index
+        perform a binary search on the number itself
         """
 
+        l,r = 0,n
+        result = -1
 
-        """
-        how to find the very first bad version?
-        - Use API call to check if its bad
-        - binary search to minimize runtime
-
-        bad = 2
-        1 2 3 4 5 6 7
-              ^
-        """
-        result = 0
-        prevVersion = 0
-        l,r = 1,n
-
-        while l<r:
+        while l<=r:
             mid = (l+r)//2
-
             if isBadVersion(mid):
-                r = mid
+                r = mid - 1
             else:
                 l = mid + 1
+            if isBadVersion(mid):
+                result = mid
 
-        return l
+        return result
