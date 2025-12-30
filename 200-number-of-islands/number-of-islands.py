8fa0bc47d26b684febad99604efbddcb764ab618
Brandon Viaje <viajebrandon@gmail.com>
2025-12-30 11:12:35 -0500

    Time: 220 ms (72.12%) | Memory: 27.5 MB (97.76%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -4,60 +4,34 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
-        """
-        count the number of islands by doing a DFS to explore all horizontally and vertically adjacent cells
-        DFS for graphs using matrix would be O(m*n) as you have to iterate through the whole grid
-        U can either keep track of the coords of each cell by using a set or overwriting it on the grid
-        """
-
-        # DFS so that we can mark all neighboring cells
+        
         def DFS(row,col):
-            # mark the starting point as seen
-            grid[row][col] = "2"
-
-            # stack for DFS
+            # set as seen
+            grid[row][col] = "0"
             stack = [(row,col)]
 
-            # while the stack isnt empty, explore all neighboring cells and mark it as seen so you dont
-            # encounter any infinite loops
+            # run dfs while stack has stuff to process still
             while stack:
-                cand_row,cand_col = stack.pop()
-
-                # neighboring cells can only be vertically and horizontally adjacent to each other
-                neighbs = [
-                    (cand_row - 1,cand_col),
-                    (cand_row + 1,cand_col),
-                    (cand_row,cand_col - 1),
-                    (cand_row,cand_col + 1)
-                ]
-
-                # this would only be O(1) since you have a fixed size of 4 always
-                for nr,nc in neighbs:
-                    # what do we have to check for it?
-                    # bounds check, check if its water or seen
-
-                    if 0<=nr<ROWS and 0<=nc<COLS and grid[nr][nc] == "1":
-                        # add cell to the stack to explore even further, then mark it as seen
-                        stack.append((nr,nc))
-                        grid[nr][nc] = "2"
+                cr,cc = stack.pop()
+                # iterate through neighbors, add to stack to process them even further
+                for neighb_row, neighb_col in [(cr+1,cc),(cr-1,cc),(cr,cc+1),(cr,cc-1)]:
+            
+                    # bounds check, also check if neighboring cell is a land cell
+                    if 0<=neighb_row<ROWS and 0<=neighb_col<COLS and grid[neighb_row][neighb_col] == "1":
+                        # add to stack to process even further
+                        stack.append((neighb_row,neighb_col))
+                        # set as seen
+                        grid[neighb_row][neighb_col] = "0"
+     
 
         ROWS = len(grid)
         COLS = len(grid[0])
         islands = 0
-
-        for row in range(ROWS):
-            for col in range(COLS):
-                # if its equal to 1, it is an unvisited island
-                if grid[row][col] == "1":
-                    DFS(row,col) # keep track of all valid islands cells thats part of the island rn
-                    islands += 1 # iterate islands 
+        for i in range(ROWS):
+            for j in range(COLS):
+                # run a DFS so that you can add all the island cells
+                if grid[i][j] == "1":
+                    DFS(i,j)
+                    islands += 1
 
         return islands
-
-        """
-        [
-        ["1","1","1","1","0"],
-        ["1","1","0","1","0"],
-        ["1","1","0","0","0"],
-        ["0","0","0","0","0"]]
-        """
