705039e3fa7e8e9a288c8df9eb721cd35fff41a0
bvftw <viajebrandon@gmail.com>
2026-02-12 09:40:54 -0500

    Time: 7 ms (13.63%) | Memory: 20.6 MB (47.04%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,15 +1,39 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
+        """
+        remove duplicates in place
 
-        # Pointer for the position to write the next unique element
-        write = 1
+        array is already sorted in increasing order
 
-        # Traverse from the second element
-        for read in range(1, len(nums)):
-            if nums[read] != nums[read - 1]:
-                nums[write] = nums[read]
-                write += 1
+        we can have a set to keep track of membership,
+        if it has already been seen in the set, dont add to result
+        else add to result
 
-        return write
+        [1,2,2]
+         ^
+         ^
+
+        [0,1,2,3,4,1,2,1,3,4]
+                           ^
+                   ^
+         {0,1}
+        """
+
+        seen = set()
+        unique = 0
+
+        for i in range(len(nums)):
+            # check if it is a duplicate
+            if nums[i] in seen:
+                continue
+            
+            # if not dupe, add to set swap val update ptr
+            seen.add(nums[i])
+            nums[i], nums[unique] = nums[unique], nums[i]
+            unique += 1
+
+        # get rid of duplicates
+        for _ in range(len(nums) - unique):
+            nums.pop()
+        
+        return len(nums)
