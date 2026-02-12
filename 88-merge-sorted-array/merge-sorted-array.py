3175a607effa6478228fc4982af7468674ecf29c
bvftw <viajebrandon@gmail.com>
2026-02-12 09:15:39 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (27.14%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -2,33 +2,40 @@ class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
+
+        both arrays are always sorted in increasing order
+
+        since they are sorted in increasing order, build the list backwards, always put the max out of the two at the back
+        shift ptr depending on what is bigger 
+
+        [1,1, 2, 2, 3,3]
+         ^
+           ^
+        [1,2,3]
+         ^
         """
 
-        # build the array backwards since it's sorted
         p1 = m - 1
-        p2 = n - 1
+        p2 = n -1
         p = len(nums1) - 1
 
-        #while there are still elements to place
+        # build array backwards, extract max 
         while p1 >= 0 and p2 >= 0:
-            #start with the max values at the end of the array 
             if nums1[p1] > nums2[p2]:
                 nums1[p] = nums1[p1]
-                p1 -= 1 #move nums1 pointer to the left
+                p -= 1
+                p1 -= 1
             else:
                 nums1[p] = nums2[p2]
-                p2 -= 1 #move nums2 pointer to the left
+                p -= 1
+                p2 -= 1
 
-            p -=1 #move the main pointer after
-
-        #remaining elements of nums2 copy them over
-        while p2>= 0:
+        # add the leftover numbers in p2
+        while p2 >= 0:
             nums1[p] = nums2[p2]
+            p -= 1
             p2 -= 1
-            p -=1
 
-        # T O(m + n) S O(1)
-        """
-        Where m is the length of nums1 and n is the length of nums2
-        O(1) memory because it is in place
-        """
+        
+
+
