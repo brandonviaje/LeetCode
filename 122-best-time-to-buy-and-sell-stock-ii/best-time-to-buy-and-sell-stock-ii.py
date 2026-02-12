08142e4402e80c0171157df198d895da1ce652ff
bvftw <viajebrandon@gmail.com>
2026-02-12 10:55:16 -0500

    Time: 2 ms (50.81%) | Memory: 20.6 MB (12.61%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,25 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        
-        """
-        You can choose to buy a stock on a certain day, or skip.
-        You can choose to sell a stock on a certain day, or skip.
-        
-        The goal is to maximize total profit — you can make as many transactions 
-        as you want
-
-        Whenever today's price is higher than yesterday's, we can treat that as 
-        part of a profitable transaction, effectively capturing all upward trends.
-
-        Dynamic Programming Intuition:
-        - dp[i][0]: max profit on day i when not holding a stock
-        - dp[i][1]: max profit on day i when holding a stock
-        """
-
         profit = 0
 
+        # start at 1 to prevent out of bounds error
         for i in range(1, len(prices)):
+            # if current price is greater than prev day
             if prices[i] > prices[i - 1]:
-                profit += prices[i] - prices[i - 1]
+                profit += prices[i] - prices[i - 1] # add profit for those two
         
         return profit
