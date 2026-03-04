d4ebae852b1f6a40514f1c8a22495b834ea6729f
bvftw <viajebrandon@gmail.com>
2026-03-04 11:39:14 -0500

    Time: 0 ms (100.00%) | Memory: 20.6 MB (81.15%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,60 +1,49 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        """
-        must run in logn, binary search implied
-        - you are also given a sorted array, so binary search will help to eliminate search space
+        left, right = -1, -1
+        l,r = 0,len(nums)-1
 
-        when you get a number==target, you need to check if its the first occurence (leftmost index) of that num
-           
-        we can do a a two pass binary search:
-        1. first binary search is to get the very first occurence (leftmost) of the target index
-        2. second is to get very last index of the target num
+        # find the first position first
+        while l <= r:
+            mid = (l+r)//2
 
-        first BS + second BS:  logn + logn = O(logn)
+            if nums[mid] == target:
+                left = mid # store as potential leftmst occurence of number
+                r = mid - 1 # update ptr to search the left side to check if there is leftmost occurence
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
 
-        [5,7,7,8,8,10]
-               ^
-                    ^
+        l,r = 0,len(nums)-1
 
-        0 + 5 // 2 = 2
-        7 // 2 = 3
+        # do a second binary search to find the rightmost occurence of the target
+        while l<=r:
+            mid = (l+r)//2
 
-        8 // 2 = 4
-        """
+            if nums[mid] == target:
+                right = mid # store potential rightmost index
+                l = mid + 1 # explore the right side of the array to find the rightmost
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
 
-        def leftMost():
-            l, r = 0, len(nums)-1
-            left = -1
+        return [left,right] if left != -1 and right != -1 else [-1,-1]
 
-            while l<=r:
-                mid = (l+r)//2
-                # tighten lower bound
-                if nums[mid] >= target:
-                    r = mid -1
-                else:
-                    l = mid + 1
-                if nums[mid] == target:
-                    left = mid
 
-            return left
+    """
+    [5,7,7,8,8,10]
+           ^
+                ^
+    target = 8
 
-        def rightMost():
-            l, r = 0, len(nums)-1
-            right = -1
+    [1]
+     ^
+     ^
 
-            while l<=r:
-                mid = (l+r)//2
-                # tighten upper bound
-                if nums[mid] <= target:
-                    l = mid + 1
-                else:
-                    r = mid - 1
-                if nums[mid] == target:
-                    right = mid
+    [0,0]
 
-            return right
-        
-        return [leftMost(), rightMost()]
-
-        # T O(logn)
-        # S O(1)
+    T O(log n)
+    S O(1)
+    """
