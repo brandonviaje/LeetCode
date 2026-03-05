870ef9992b455842e80c85407939497d6615bc86
bvftw <viajebrandon@gmail.com>
2026-03-05 09:15:58 -0500

    Time: 8 ms (6.31%) | Memory: 21.3 MB (90.57%) - LeetSync

--- 252-meeting-rooms/meeting-rooms.py
+++ 252-meeting-rooms/meeting-rooms.py
@@ -1,19 +1,20 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         """
-        a person cannot attend a meeting if one of the intervals[i][0] is < intervals[i-1][1]
-        sort the array by starting times to make it easier to track and return false
-        """
-        intervals.sort()
+        a person can attend a meeting if the start of a meeting does not interfere with any other meeting intervals
+        that means that the start time can not be in between the end interval of another meeting. they must all be different
 
-        if len(intervals) <= 1:
-            return True
-        
-        # check if any conflicting meeting intervals
+        to do this efficiently, we can sort the array by start times from earliest time to latest start time. then we check if 
+        the current meeting is <= end of the prev meeting. if it is, then you can not attend all meetings.
+
+        return true at the end which means none of the times conflict with each other
+        """
+
+        intervals.sort()
+        print(intervals)
+
         for i in range(1,len(intervals)):
             if intervals[i][0] < intervals[i-1][1]:
                 return False
-
-        print(intervals)
 
         return True
