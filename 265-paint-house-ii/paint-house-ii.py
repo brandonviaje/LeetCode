1e15ed94b98ac9929caf4712663be57bb0fa759c
bvftw <viajebrandon@gmail.com>
2026-03-10 14:15:35 -0400

    Time: 145 ms (5.01%) | Memory: 24.9 MB (12.12%) - LeetSync

--- 265-paint-house-ii/paint-house-ii.py
+++ 265-paint-house-ii/paint-house-ii.py
@@ -1,42 +1,36 @@
 class Solution:
     def minCostII(self, costs: List[List[int]]) -> int:
         """
-        given a cost matrix where cost[i][j] represents cost to paint house i with color j
-        we want to return the min cost to paint all houses with different colors. no two houses can have the same
-        adjacent color. 
+        want to explroe the different costs of each color of a house, and get the min cost
 
-        options:
-        - can either paint current house current color
-        - skip current house and paint same color
-        - paint current house a different color
-        - skip current house and paint next house different color
+        subproblem:
+        - take the current cost of house and color
+        - explore next house based on this color
         """
+        memo = {}
         n = len(costs)
         k = len(costs[0])
-        memo = {}
 
         def DP(house,color):
             # check if precomputed already
-            if(house,color) in memo:
+            if (house,color) in memo:
                 return memo[(house,color)]
 
-            # base case
             if house == n:
                 return 0
-
-            result = float('inf')
             
-            # go through every diff color than prev
-            for i in range(k):
+            result = float('inf')
+            # explore each color
+            for col in range(k):
                 # skip same color
-                if i == color:
+                if col == color:
                     continue
-
-                # add current cost and explore the next house
-                temp = costs[house][i] + DP(house+1,i)
-                result = min(result, temp) # capture min cost to paint houses diff colors
+                # recurrence relation
+                temp = costs[house][col] + DP(house+1,col)
+                result = min(result, temp)
                 memo[(house,color)] = result
 
             return memo[(house,color)]
+
+        return DP(0,-1)
                 
-        return DP(0,-1) # start at house 0, -1 for curr color so u can explore first color
