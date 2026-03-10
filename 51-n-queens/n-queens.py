731262515342bf40e7272c84f23441ef86666263
bvftw <viajebrandon@gmail.com>
2026-03-10 11:27:53 -0400

    Time: 7 ms (96.09%) | Memory: 19.7 MB (64.33%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,37 +1,35 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         cols = set()
-        posDiag = set() # row + col
-        negDiag = set() # row - col
+        posDiag = set()
+        negDiag = set()
 
-        # create result and board state
         result = []
-        board = [['.'] * n for i in range(n)]
+        board = [["."] * n for _ in range(n)]
 
-        # iterate through board row by row
         def backtrack(row):
-            # base case, if reach end of board we found a distinct puzzle
+            # base case
             if row == n:
-                result.append(["".join(row) for row in board]) # add board to result
+                result.append(["".join(row) for row in board])
                 return
             
-            # iterate through each col in row
+            # iterate current col 
             for col in range(n):
-                # check if a queen occupies this diag/cols
-                if col in cols or (row - col) in negDiag or (row+col) in posDiag:
+                # skip current square if queen already on that col/diag
+                if col in cols or (row-col) in negDiag or (row+col) in posDiag:
                     continue
-                
+
                 cols.add(col)
-                negDiag.add(row - col)
-                posDiag.add(row + col)
+                negDiag.add(row-col)
+                posDiag.add(row+col)
                 board[row][col] = "Q"
 
-                backtrack(row+1) # recurse to next row
+                backtrack(row+1) # recurse further
 
-                # backtrack/undo
+                # undo/backtrack
                 cols.remove(col)
-                negDiag.remove(row - col)
-                posDiag.remove(row + col)
+                negDiag.remove(row-col)
+                posDiag.remove(row+col)
                 board[row][col] = "."
 
         backtrack(0)
