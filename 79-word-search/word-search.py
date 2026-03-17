a7e63062ec37520515a508d4749eeec46dafc438
bvftw <viajebrandon@gmail.com>
2026-03-17 13:56:58 -0400

    Time: 3472 ms (69.64%) | Memory: 19.5 MB (77.85%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,42 +1,37 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        ROWS = len(board)
-        COLS = len(board[0])
+        """
+        backtrack
+        """
+        m, n = len(board), len(board[0])
 
-        # DFS every cell check if it equals to word at index
-        def dfs(row,col,index):
+        def backtrack(index, row, col):
             # base case
-            if index == len(word):
-                return True 
-            # check if out of bounds, marked as visited, or if equals to word at index
-            if (row < 0 or col < 0 or row >= ROWS or col >= COLS or board[row][col] == '.' or board[row][col] != word[index]):
+            if index >= len(word):
+                return True
+            if row < 0 or row >= m or col < 0 or col >= n:
                 return False
+            if board[row][col] == ".":
+                return False
+            if board[row][col] != word[index]:
+                return False
+            
+            board[row][col] = "."
 
-            # mark in place as visited
-            board[row][col] = '.'
+            # recurse to check if next char matches
+            found = (backtrack(index+1,row + 1, col) or 
+                    backtrack(index+ 1, row -1 , col) or 
+                    backtrack(index+1,row,col+1) or 
+                    backtrack(index+1,row,col-1))
 
-            # explore all neighbours of cell check if it equals word at next index
-            result = (dfs(row-1, col,index+1) or
-                      dfs(row, col+1,index+1) or
-                      dfs(row+1, col,index+1) or
-                      dfs(row, col-1,index+1))   
+            # undo/backtrack
+            board[row][col] = word[index]
+            return found
 
-            board[row][col] = word[index] # switch back after exploring
-
-            return result
-
-        for row in range(ROWS):
-            for col in range(COLS):
-                if dfs(row,col,0): 
+        for i in range(m):
+            for j in range(n):
+                # check if word is found
+                if backtrack(0,i,j):
                     return True
 
         return False
-
-        """
-        - bruteforce the combinations
-        - DFS every cell
-        - base case if index same as len of word
-        [A,B,C,E]
-        [S,F,C,S]
-        [A,D,E,E] checks each cell if it starts with our first letter
-        """
