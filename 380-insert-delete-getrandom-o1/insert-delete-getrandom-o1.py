b4e90bd063b3f188c835158d53bf83563605589c
bvftw <viajebrandon@gmail.com>
2026-03-23 17:38:12 -0400

    Time: 132 ms (30.57%) | Memory: 57 MB (58.05%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,40 +1,35 @@
+from random import randint
 class RandomizedSet:
 
     def __init__(self):
-        self.map = {}
-        self.num = []
+        self.vals = []        
+        self.map = {} # map val to index
 
     def insert(self, val: int) -> bool:
-        # check if val is in hashmap
         if val in self.map:
             return False
 
-        # else add to list then map val to index
-        self.num.append(val)
-        self.map[val] = len(self.num) - 1
+        self.vals.append(val)
+        self.map[val] = len(self.vals) - 1
         return True
 
     def remove(self, val: int) -> bool:
-
         if val not in self.map:
-            return False 
+            return False
 
-        # grab index of val we are going to delete and swap with last elem in num
-        index = self.map[val]
-        lastVal = self.num[-1]
-        self.num[index] = lastVal
+        # get old index of val getting deleted
+        old_idx = self.map[val]
+        lastVal = self.vals[-1]
+        self.vals[old_idx], self.vals[-1] = self.vals[-1],self.vals[old_idx] # swap with last elem
+        self.map[lastVal] = old_idx                               # update swapped vals index
 
-        # update last values index
-        self.map[lastVal] = index
-        
-        # delete last number in list and delete val from map
-        self.num.pop()
-        del self.map[val]
+        self.vals.pop()                                                      # delete val
+        del self.map[val]                                                    # delete val in map
 
         return True
-
     def getRandom(self) -> int:
-        return random.choice(self.num)
+        randomIdx = randint(0, len(self.vals) - 1) # get random num
+        return self.vals[randomIdx]
 
 
 # Your RandomizedSet object will be instantiated and called as such:
