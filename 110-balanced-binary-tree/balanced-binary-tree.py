93f3609918f39810018da37ff002584e3244ea94
bvftw <viajebrandon@gmail.com>
2026-03-26 15:59:49 -0400

    Time: 3 ms (51.04%) | Memory: 20.5 MB (35.86%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -1,45 +1,25 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def isBalanced(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: bool
-        """
-        
-        """
-        post order traversal so that you can know the depth 
-        and then basically you can just compare by subtracting the left and right depths 
-        and checking if they dont differ no more than 1
-        """
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
 
-        if not root:
-            return True
-
-        def DFS(root):
-            # return 0 if u come across an empty node
+        def dfs(root):
+            # base case 
             if not root:
                 return 0
 
-            left = DFS(root.left)
-            # return -1 up the tree if we found an imbalance already
-            if left == -1:
-                return -1
-            right = DFS(root.right) 
-            # return -1 up the tree if we found an imbalance already
-            if right == -1:
-                return -1
+            # recurse through our subtree
+            left = dfs(root.left)
+            right = dfs(root.right)
 
-            # check if the depths are greater than 1, if it isnt then its not balanced
-            if abs(left-right) > 1:
-                return -1
+            # check if height balanced
+            if abs(left - right) > 1:
+                return float('-inf')
 
-            # return the depth
             return 1 + max(left,right)
 
-        return DFS(root) != -1
-        
+        return True if dfs(root) != float('-inf') else False
