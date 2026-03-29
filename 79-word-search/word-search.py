8bd8dd6072fb25a2ce14ba507baa852a21e75882
bvftw <viajebrandon@gmail.com>
2026-03-29 15:24:44 -0400

    Time: 3984 ms (48.41%) | Memory: 19.2 MB (96.94%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,37 +1,41 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         """
-        backtrack
-        """
-        m, n = len(board), len(board[0])
+        we are given a board of letters and must search if a word exists
+        we must return a boolean indicating if a word is present in this matrix
 
-        def backtrack(index, row, col):
-            # base case
+        idea: traverse each cell, perform a DFS and check if you can build the word
+        if you cant, move onto the next cell until you reach the end of the cell
+        """
+
+        def DFS(row,col,index):
+            # base case: if our index reaches out of bound we have found the word
             if index >= len(word):
                 return True
-            if row < 0 or row >= m or col < 0 or col >= n:
+
+            # prune: out of bounds/seen
+            if row < 0 or row >= len(board) or col < 0 or col >= len(board[0]) or board[row][col] == '.':
                 return False
-            if board[row][col] == ".":
-                return False
+
+            # prune: char mismatch
             if board[row][col] != word[index]:
                 return False
-            
-            board[row][col] = "."
 
-            # recurse to check if next char matches
-            found = (backtrack(index+1,row + 1, col) or 
-                    backtrack(index+ 1, row -1 , col) or 
-                    backtrack(index+1,row,col+1) or 
-                    backtrack(index+1,row,col-1))
+            # set as '.' to denote that we seen this
+            board[row][col] = '.'
 
-            # undo/backtrack
-            board[row][col] = word[index]
-            return found
+            # recurse further to check if we can find the word from here
+            match = (DFS(row+1,col,index+1) or DFS(row-1,col,index+1) 
+                        or DFS(row,col+1,index+1) or DFS(row,col-1,index+1))
+           
+            board[row][col] = word[index]  # undo/backtrack
+            return match
 
-        for i in range(m):
-            for j in range(n):
-                # check if word is found
-                if backtrack(0,i,j):
+        # iterate through the board
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                # check if you can find the word from here
+                if DFS(i,j,0):
                     return True
 
         return False
