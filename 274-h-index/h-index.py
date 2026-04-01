39e70f713a8103b3666185fd73bf1d4add517acc
bvftw <viajebrandon@gmail.com>
2026-04-01 11:16:39 -0400

    Time: 0 ms (100.00%) | Memory: 19.2 MB (95.71%) - LeetSync

---
+++ 274-h-index/h-index.py
@@ -0,0 +1,21 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+
+        n = len(citations)
+        l,r = 0,n-1
+
+        while l<=r:
+            mid = (l+r)//2
+
+            # check if we found h-index
+            if (n-mid) == citations[mid]:
+                return n - mid
+
+            # prune search space 
+            if (n-mid) < citations[mid]:
+                r = mid -1
+            else:
+                l = mid+1
+
+        return n - l
