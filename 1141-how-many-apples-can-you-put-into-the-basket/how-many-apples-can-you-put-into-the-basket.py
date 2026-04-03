a68a82ae0981509b32c1732e85698bf9481a2f8a
bvftw <viajebrandon@gmail.com>
2026-04-02 22:29:51 -0400

    Time: 7 ms (40.96%) | Memory: 19.2 MB (98.64%) - LeetSync

--- 1141-how-many-apples-can-you-put-into-the-basket/how-many-apples-can-you-put-into-the-basket.py
+++ 1141-how-many-apples-can-you-put-into-the-basket/how-many-apples-can-you-put-into-the-basket.py
@@ -1,29 +1,19 @@
 class Solution:
     def maxNumberOfApples(self, weight: List[int]) -> int:
         """
-        what we can do is check if adding this apple exceeds the limit
-
-        if it doesn't then we can update our length by doing max(result, (i - l) + 1)
-
-        brute force:
-        try all combinations of weight and then get length from j-i
-
-        we can sort the array, and make a prefix sum arr
-        then as long as prefix[i] <= 5000, update a result variable max(result,i+1)
+        sort the weight array, to get smaller weights first
+        we want to get smaller weights first so that we can get as much apples in the basket
         """
 
         weight.sort()
-        prefix = [0] * len(weight)
-        prefix[0] = weight[0]
+        total = 0
+        count = 0
 
-        for i in range(1,len(weight)):
-            prefix[i] = prefix[i-1] + weight[i]
+        for w in weight:
+            if total + w > 5000:
+                break
 
-        result = float('-inf')
-        print(prefix)
+            total += w
+            count += 1
 
-        for i in range(len(prefix)):
-            if prefix[i] <= 5000:
-                result = max(result, i+1)
-
-        return result
+        return count
