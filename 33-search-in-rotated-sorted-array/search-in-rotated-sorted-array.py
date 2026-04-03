5bfbffa3972fa14a595583c5b0c9dc57b04a7db2
bvftw <viajebrandon@gmail.com>
2026-04-03 11:41:45 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (86.68%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,30 +1,34 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l,r = 0 , len(nums)-1
+        """
+        check which side of the array is sorted
+        if its the left side, check if ur target resides in left side, if it does then search left side else search right
+        
+        if left side isn't sorted, then right side must be sorted so u check if ur target resides in the right side,
+        if it does then search right side more else search left
+        """
 
-        while l <= r:
+
+        result = -1
+        l,r = 0,len(nums)-1
+
+        while l<=r:
             mid = (l+r)//2
 
-            # if nums at middle is target return
             if nums[mid] == target:
-                return mid
+                result = mid
+                return result
 
             # check if left side is sorted
             if nums[l] <= nums[mid]:
-                # check if its in the left side else we explore the right
-
-                # check if it is in the range of the left, if it isn't then it must be in the right side
-                if target < nums[l] or target > nums[mid]:
-                    l = mid + 1
-                else: # else it must be in the left side, update right pointer
+                if nums[l] <= target <= nums[mid]:
                     r = mid - 1
+                else:
+                    l = mid + 1
             else:
-                # check if target is in the right side of the array, if it isnt, it must be on the left side
-                if target < nums[mid] or target > nums[r]:
-                    r = mid - 1
-                else: # else it must be in the right side, update left pointer
+                if nums[mid] <= target <= nums[r]:
                     l = mid + 1
+                else:
+                    r = mid - 1
 
-        return -1
-
-        # T O(logn) S O(1)
+        return result
